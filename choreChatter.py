dbe360896b618c0325fa451940b1f3b57a56744b
Maryam Bouamama <maryam.bouamama@sjsu.edu>
2022-03-11 16:19:08 -0800

    code to load env var file

--- choreChatter.py
+++ choreChatter.py
@@ -2,3 +2,7 @@ import slack
 import os
 from pathlib import pathlib
 from dotenv import load_dotenv
+
+#load environment variable file
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
