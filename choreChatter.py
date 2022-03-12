43cac7348e7414aa802884efc0f5459c51ab1f53
Maryam Bouamama <maryam.bouamama@sjsu.edu>
2022-03-11 16:21:17 -0800

    load token value as a variable(for security)

--- choreChatter.py
+++ choreChatter.py
@@ -6,3 +6,7 @@ from dotenv import load_dotenv
 #load environment variable file
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
+
+#load token value, pass as token(stores token through environment variable)
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+
