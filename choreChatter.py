18a55ae069c88c3f7cc2cfa7349763e2150d774b
Maryam Bouamama <maryam.bouamama@sjsu.edu>
2022-03-11 16:28:46 -0800

    changed channel name

--- choreChatter.py
+++ choreChatter.py
@@ -11,5 +11,5 @@ load_dotenv(dotenv_path=env_path)
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 
 #Bot posts message to specified chat
-client.chat_postMessage(channel='#general')
+client.chat_postMessage(channel='#tidy-up')
 
